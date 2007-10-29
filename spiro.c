b4c7d5c6f37f224694f0c88719c14a61c06c5f1e
George Williams <gww@silcom.com>
2007-10-29 00:18:32 +0000

    Oh. VERBOSE is defined in the file. Comment it out.
    git-svn-id: https://libspiro.svn.sourceforge.net/svnroot/libspiro@5 d59fb4bf-6b3d-0410-b1f7-f99c41b88066

--- spiro.c
+++ spiro.c
@@ -744,7 +744,9 @@ spiro_iter(spiro_seg *s, bandmat *m, int *perm, double *v, int n)
 	n_invert = nmat;
 	j = 0;
     }
+/*
 #define VERBOSE
+*/
 #ifdef VERBOSE
     for (i = 0; i < n; i++) {
 	int k;
