f5894872691ae023a6e67f01f4b950287e9e3cc1
Joe Da Silva <digital@joescat.com>
2013-06-28 08:08:43 -0700

    Worthwhile comments thanks to Øyvind Kolås, 2008-10-19 14:52:20
    Comments for _bezctx structure duplicated from gegl commit on 2008-10-19.

--- bezctx.h
+++ bezctx.h
@@ -3,11 +3,21 @@
 #include "bezctx_intf.h"
 
 struct _bezctx {
+    /* Called by spiro to start a contour */
     void (*moveto)(bezctx *bc, double x, double y, int is_open);
+
+    /* Called by spiro to move from the last point to the next one on a straight line */
     void (*lineto)(bezctx *bc, double x, double y);
+
+    /* Called by spiro to move from the last point to the next along a quadratic bezier spline */
+    /* (x1,y1) is the quadratic bezier control point and (x2,y2) will be the new end point */
     void (*quadto)(bezctx *bc, double x1, double y1, double x2, double y2);
+
+    /* Called by spiro to move from the last point to the next along a cubic bezier spline */
+    /* (x1,y1) and (x2,y2) are the two off-curve control point and (x3,y3) will be the new end point */
     void (*curveto)(bezctx *bc, double x1, double y1, double x2, double y2,
 		    double x3, double y3);
+
     void (*mark_knot)(bezctx *bc, int knot_idx);
 };
 #endif
