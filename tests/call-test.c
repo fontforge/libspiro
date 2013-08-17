29ec7e45bdf3ca3f535c9a2e48e004c84c7079c8
Joe Da Silva <digital@joescat.com>
2013-08-17 11:34:33 -0700

    Run "make check" to test libspiro as a library. Test spiro_to_bpath(() 3x

--- tests/call-test.c
+++ tests/call-test.c
@@ -25,6 +25,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 #include <sys/time.h>		/* for gettimeofday */
 
 #include "spiroentrypoints.h"	/* call spiro through here */
+#include "bezctx.h"		/* bezctx structure */
 
 #include "spiro-config.h"	/* for ./configure test settings like VERBOSE */
 
@@ -58,7 +59,7 @@ void load_test_curve(spiro_cp *spiro, int *nextknot, int c) {
 	{0, 0, 'z'}
     };
     int knot0[] = {
-	1, 1, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 0
+	1, 1, 3, 3, 2, 3, 1, 1, 1, 3, 3, 2, 3, 7, 1, 1
     };
     spiro_cp path1[] = {
 	{80, 738, '{'},
@@ -69,7 +70,7 @@ void load_test_curve(spiro_cp *spiro, int *nextknot, int c) {
 	{467, 231, '}'}
     };
     int knot1[] = {
-	1, 4, 4, 4, 4, 0
+	3, 4, 2, 3, 2, 0
     };
     spiro_cp path2[] = { /* this does many iterations */
 	{233, 143, '{'},
@@ -78,7 +79,7 @@ void load_test_curve(spiro_cp *spiro, int *nextknot, int c) {
 	{444, 141, '}'}
     };
     int knot2[] = {
-	1, 4, 4, 0
+	2, 1, 9, 0
     };
     int i;
 
@@ -101,10 +102,42 @@ void load_test_curve(spiro_cp *spiro, int *nextknot, int c) {
 }
 int cl[] = {16, 6, 4};
 
+/* Provide bare-bones do-nothing functions for testing. This only */
+/* printf values that would normally be handled by user programs. */
+void test_moveto(bezctx *z, double x, double y, int is_open) {
+    printf("test_moveto(%g,%g)_%d\n",x,y,is_open);
+}
+void test_lineto(bezctx *z, double x, double y) {
+    printf("test_lineto(%g,%g)\n",x,y);
+}
+void test_quadto(bezctx *z, double x1, double y1, double x2, double y2) {
+    printf("test_quadto(%g,%g, %g,%g)\n",x1,y1,x2,y2);
+}
+void test_curveto(bezctx *z, double x1, double y1, double x2, double y2,
+		  double x3, double y3) {
+    printf("test_curveto(%g,%g, %g,%g, %g,%g)\n",x1,y1,x2,y2,x3,y3);
+}
+void test_mark_knot(bezctx *bc, int knot_idx) {
+    printf("test_mark_knot()_%d\n",knot_idx);
+}
+
+bezctx *new_bezctx_test(void) {
+    bezctx *result = malloc(1*sizeof(bezctx));
+
+    result->moveto = test_moveto;
+    result->lineto = test_lineto;
+    result->quadto = test_quadto;
+    result->curveto = test_curveto;
+    result->mark_knot = test_mark_knot;
+    return result;
+}
+
 int test_curve(int c) {
     spiro_cp spiro[16];
-    int nextknot[16];
+    int nextknot[17];
     spiro_seg *segs = NULL;
+    bezctx *bc;
+    int i;
 
     /* Load sample data so that we can see if library is callable */
     load_test_curve(spiro,nextknot,c);
@@ -116,6 +149,18 @@ int test_curve(int c) {
 	return -1;
     }
 
+    /* Quick visual check shows X,Y knots match with each pathN[] */
+    for (i=0; i < cl[c]; i++) {
+	printf("curve %d, line %d, x=%f y=%f t=%c bend=%f ch=%f th=%f l=%f \n",c,i,segs[i].x,segs[i].y,segs[i].ty,segs[i].bend_th,segs[i].seg_ch,segs[i].seg_th,segs[i].l);
+    }
+
+    /* Quick visual check shows X,Y knots match with each pathN[] */
+    printf("testing spiro_to_bpath() using data from run_spiro(data=path%d[],len=%d).\n",c,cl[c]);
+    bc = new_bezctx_test();
+    spiro_to_bpath(segs,cl[c],bc);
+
+    free(bc);
+    free(segs);
     return 0;
 }
 
