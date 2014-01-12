69a44653edee463e64a7857795cdadce35b496b4
Joe Da Silva <digital@joescat.com>
2014-01-11 20:38:18 -0800

    [libspiro] Update license headers (issue#8).
    According to COPYING, libspiro is released under the term of GPLv3+,
    but in the license headers on top of *.c, *.java and bezctx.md are
    still saying GPLv2+.
    
    ...updated files that were updated from libspiro 20071029
    left unmodified files untouched as-is.

--- tests/unit-test.c
+++ tests/unit-test.c
@@ -1,4 +1,23 @@
-/* Unit test for testing spiro.c internal routines */
+/* Unit test for testing spiro.c internal routines
+Copyright (C) 2007 Raph Levien   (original code was from spiro.c)
+Copyright (C) 2013 Jose Da Silva (moved from spiro.c, plus edits)
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 3
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+02110-1301, USA.
+
+*/
 
 #include <math.h>
 #include <stdlib.h>
